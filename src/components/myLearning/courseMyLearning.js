ae04b2a073bf58b132ed1e14583ed48e0b7c0e62
michael gabay <michaelgabay770@gmail.com>
2022-09-14 05:36:41 +0300

    added design

--- src/components/myLearning/courseMyLearning.js
+++ src/components/myLearning/courseMyLearning.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import { Grid, Box, Typography } from "@mui/material"
+import { Grid, Box, Typography, Rating } from "@mui/material"
 import "./myLearning.css"
 import { FiMoreVertical } from "react-icons/fi"
 import playPic from "../../assets/images/myLearning/playPic.png"
 import noImagCoursePic from "../../assets/images/coursePage/noImageCourse.webp"
+import { useNavigate } from 'react-router-dom'
 
 const CourseMyLearning = ({ course, myLearning }) => {
     const [progres, setProgres] = useState(0)
+    const nav = useNavigate()
     useEffect(() => {
         if (myLearning) updateProgres();
+
     }, [myLearning])
     const updateProgres = () => {
         let myLearningCourse = myLearning?.find(item => item.ShortIdCourse == course.short_id);
         if (course?.lessonLength) {
             let progresPercent = Math.ceil((myLearningCourse?.lessonsViewsId.length * 100) / course?.lessonLength);
             if (progresPercent) {
-                setProgres(progresPercent)
+                setProgres(progresPercent + "%")
             }
         }
     }
@@ -29,14 +32,24 @@ const CourseMyLearning = ({ course, myLearning }) => {
                         </Box>
                     </Box>
                     <Box className='myLearning-playIcon'>
-                        <img className='myLearning-playIcon-self' src={playPic}></img>
+                        <img className='myLearning-playIcon-self' onClick={() => nav(`/course?shortId=${course.short_id}`)} src={playPic}></img>
                     </Box>
                 </Box>
                 <Typography sx={{ fontSize: "15px", fontWeight: "600", paddingTop: "8px" }} variant='subtitle2' component="h2">{course.name}</Typography>
-                <Typography variant='caption' component="h3">{`${course?.creator_id.fullName.firstName} ${course?.creator_id.fullName.lastName}`}</Typography>
+                <Typography className='myLearning-creator' variant='caption' component="h3">{`${course?.creator_id.fullName.firstName} ${course?.creator_id.fullName.lastName}`}</Typography>
                 <Box className='myLearning-progres'>
-                    <Box sx={{ width: progres, background: "blue", height: "4px" }}></Box>
+                    <Box sx={{ width: progres, background: "purple", height: "2px" }}></Box>
                 </Box>
+                <Box sx={{ paddingTop:"4px",display: "flex",flexWrap:"wrap", justifyContent: "space-between" }}>
+                    <Box >
+                        <Typography  variant='subtitle2' component="h4">{progres ? `${progres} הושלמו` : "התחל קורס"}</Typography>
+                    </Box>
+                    <Box sx={{textAlign:"end",cursor:"pointer"}} className='myLearning-rating'>
+                        <Rating  readOnly />
+                        <Typography sx={{margin:0,paddingLeft:"4px"}} variant='subtitle2' component="h4">השאר דירוג</Typography>
+                    </Box>
+                </Box>
+
             </Box>
         </Grid>
     )
--- src/components/myLearning/myLearning.js
+++ src/components/myLearning/myLearning.js
@@ -6,17 +6,19 @@ import { CircularProgress, createTheme, Container, Grid } from "@mui/material";
 import { amber, blue, grey, lime, pink, red } from '@mui/material/colors';
 import { useNavigate } from "react-router-dom";
 import CourseMyLearning from './courseMyLearning';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from '../../shared/redux/features/userSlice';
 
 export default function MyLearning() {
   const nav = useNavigate();
+  const dispatch = useDispatch()
   const { user } = useSelector(store => store.userReducer)
-  // console.log(user)
   const [myLearning, setMyLearning] = useState([]);
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     getMyLearning();
     window.scrollTo(0, 0);
+    dispatch(getUser())
   }, []);
   //get my learning Courses
   const getMyLearning = async () => {
@@ -28,8 +30,56 @@ export default function MyLearning() {
   return (
     <Container className="myLearning-container">
       <Grid container spacing={2}>
-        {myLearning?.map((course) => <CourseMyLearning key={course._id} myLearning={user.myLearning} course={course} />)}
+        {loading && myLearning ?
+          <CircularProgress sx={{ position: "absolute", right: "50%", left: "50%", top: "50%", bottom: "50%", }}
+            size={50}
+          />
+          : <>
+            {myLearning?.map((course) => <CourseMyLearning key={course._id} myLearning={user?.myLearning} course={course} />)}
+          </>}
+
       </Grid>
     </Container>
   );
 }
+
+
+
+
+// <ThemeProvider theme={custom}>
+// <AuthUser />
+// {!loading ?
+//   <div className={`${style.container} py-5`}>
+//     {myLearning.length > 0 ? myLearning.map((item) => (
+//       <div
+//         key={item._id}
+//         style={{ direction: "rtl" }}
+//         className={`mt-2 mt-md-0  d-flex flex-md-row   flex-column   justify-content-md-between ${style.course}`}
+//       >
+//         <div className="p-2   d-md-flex flex-column  align-items-center   col-md-auto col-12">
+
+//           <div className="d-flex">
+//             <Button
+//               onClick={() => nav(`/course?shortId=${item.short_id}`)}
+//               variant="contained"
+//               color="info"
+//             >
+//               לקורס- {item.name}
+//             </Button>
+//           </div>
+
+//         </div>
+//         <div className={` col-lg-4 col-md-6 ${style.imgWidth}`}>
+//           <div
+//             style={{ backgroundImage: item.img_url ? `url(${item.img_url})` : `url(${noImagCoursePic})` }}
+//             className={`${style.imgCourse}`}
+//           ></div>
+//         </div>
+//       </div>
+//     )) : <h1 className="text-center">טרם קנית קורס</h1>}
+
+//   </div>
+//   : <CircularProgress sx={{ position: 'absolute', right: '50%', left: '50%', top: '50%', bottom: '50%' }} size={50} />
+// }
+// </ThemeProvider>
+// );
